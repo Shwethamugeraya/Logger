a27862dd95e36a3d23370c7f316fc558a906a55c
Shwetha Mugeraya <smugeraya@sapient.com>
2017-03-27 12:36:01 +0530

    Committed a test framework

---
+++ Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by Shwetha Mugeraya on 3/23/17.
+//  Copyright © 2017 Shwetha Mugeraya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+
